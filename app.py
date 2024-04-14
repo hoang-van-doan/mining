e37cccdbb4d6094ed19e9019363cb58ed4ce1253
doanhv2606 <50062599+doanhv2606@users.noreply.github.com>
2024-04-14 23:18:15 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+# ENCODE BY KHANHNGUYEN9872                                 # Write your comment here! 
+# Version OBF: v2.4.0                                       # Write your comment here! 
+# Botname: @KazutoBot_bot                                   # Write your comment here! 
+# Username Obf: @KIMANHVV2 (5637336841)                     # Write your comment here! 
+# Mode ENC: python3.12 - High (main)                        # Write your comment here! 
+# Date Obf: 2024-03-09 13:31:22 (UTC)                       # Write your comment here! 
